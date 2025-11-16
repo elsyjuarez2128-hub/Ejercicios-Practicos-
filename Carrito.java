650ec3b1e637f2941443ee4e79b7eaf9b69e5cb6
elsyjuarez2128-hub <elsyjuarez2128@gmail.com>
2025-11-16 17:56:05 -0600

    Create Carrito.java

---
+++ Carrito.java
@@ -0,0 +1,16 @@
+*/
+*@autor Elsy Joselyn Godoniez Juarez
+*GTID141
+*Simulación del supermercado con carritos y cajas
+  
+public class Carrito {
+    private int id;
+
+    public Carrito(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
